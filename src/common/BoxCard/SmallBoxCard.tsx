2d020b551dcfd902a12b2b3b21f9c550274135ed
Juan De la Cruz <juan.carlos.cc@outlook.com>
2024-10-12 13:43:33 -0600

    Make recent drops tabable

--- src/common/BoxCard/SmallBoxCard.tsx
+++ src/common/BoxCard/SmallBoxCard.tsx
@@ -4,18 +4,20 @@ import Title4 from "../Title4";
 
 const SmallBoxCard: FC<Box> = ({ imageUrl, name, description, creator }) => {
   return (
-    <article className="flex bg-slate-800 rounded-lg max-h-20">
-      <img
-        className="h-16 ml-10 my-auto"
-        src={imageUrl}
-        alt={`${name} image`}
-      />
-      <header className="w-60 p-2.5">
-        <Title4>{name}</Title4>
-        <p className="opacity-70 text-xs">{description}</p>
-        <span className="text-xs">{creator}</span>
-      </header>
-    </article>
+    <a href="#">
+      <article className="flex bg-slate-800 rounded-lg max-h-20">
+        <img
+          className="h-16 ml-10 my-auto"
+          src={imageUrl}
+          alt={`${name} image`}
+        />
+        <header className="w-60 p-2.5">
+          <Title4>{name}</Title4>
+          <p className="opacity-70 text-xs">{description}</p>
+          <span className="text-xs">{creator}</span>
+        </header>
+      </article>
+    </a>
   );
 };
 
