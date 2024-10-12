910294431768ffad8e20c03fa9dbdd1071c4bb74
Juan De la Cruz <juan.carlos.cc@outlook.com>
2024-10-12 13:04:23 -0600

    Allow to tab across boxes

--- src/common/BoxCard/BoxCard.tsx
+++ src/common/BoxCard/BoxCard.tsx
@@ -6,21 +6,23 @@ import formatPrice from "../../utils/formatPrice";
 
 const BoxCard: FC<Box> = ({ name, description, price, state, imageUrl }) => {
   return (
-    <article className="rounded-lg bg-slate-800 p-2.5 flex flex-col justify-between min-h-60 min-w-44 md:min-h-72 md:min-w-56">
-      <div className="flex justify-between items-start">
-        <Badge variant="dark">{formatPrice(price)}</Badge>
-        {state === "new" && (
-          <Badge variant="alert" fontSize="xs">
-            {"New"}
-          </Badge>
-        )}
-      </div>
-      <img className="ml-auto" src={imageUrl} alt={`${name} image`} />
-      <header className="bg-black rounded-lg p-2.5">
-        <Title4>{name}</Title4>
-        <p className="opacity-70 text-xs">{description}</p>
-      </header>
-    </article>
+    <a href="#">
+      <article className="rounded-lg bg-slate-800 p-2.5 flex flex-col justify-between min-h-60 min-w-44 md:min-h-72 md:min-w-56">
+        <div className="flex justify-between items-start">
+          <Badge variant="dark">{formatPrice(price)}</Badge>
+          {state === "new" && (
+            <Badge variant="alert" fontSize="xs">
+              {"New"}
+            </Badge>
+          )}
+        </div>
+        <img className="ml-auto" src={imageUrl} alt={`${name} image`} />
+        <header className="bg-black rounded-lg p-2.5">
+          <Title4>{name}</Title4>
+          <p className="opacity-70 text-xs">{description}</p>
+        </header>
+      </article>
+    </a>
   );
 };
 
