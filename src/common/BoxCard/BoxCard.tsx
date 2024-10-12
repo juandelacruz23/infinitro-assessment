43e0110820d3783a606ad1f5c9060647248b0de4
Juan De la Cruz <juan.carlos.cc@outlook.com>
2024-10-12 12:15:19 -0600

    Change fontsize on BoxCard badge

--- src/common/BoxCard/BoxCard.tsx
+++ src/common/BoxCard/BoxCard.tsx
@@ -7,9 +7,13 @@ import formatPrice from "../../utils/formatPrice";
 const BoxCard: FC<Box> = ({ name, description, price, state, imageUrl }) => {
   return (
     <article className="rounded-lg bg-slate-800 p-2.5 flex flex-col justify-between min-h-60 min-w-40 md:min-h-72 md:min-w-56">
-      <div className="flex justify-between">
+      <div className="flex justify-between items-start">
         <Badge variant="dark">{formatPrice(price)}</Badge>
-        {state === "new" && <Badge variant="alert">{"New"}</Badge>}
+        {state === "new" && (
+          <Badge variant="alert" fontSize="xs">
+            {"New"}
+          </Badge>
+        )}
       </div>
       <img className="ml-auto" src={imageUrl} alt={`${name} image`} />
       <header className="bg-black rounded-lg p-2.5">
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "2xs": "0.5rem",
+      },
+    },
   },
   plugins: [],
 };
